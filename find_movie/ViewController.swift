0a45b21facea382b36fddacc51eee9c507b59cfe
student <student@mac18.local>
2019-05-08 17:29:30 +0200

    Init

---
+++ find_movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  find_movie
+//
+//  Created by student on 08.05.2019.
+//  Copyright © 2019 PG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
