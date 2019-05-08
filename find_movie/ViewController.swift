e7307aeb4b0d12e71b3ae26800651d8a53bf086d
student <student@mac18.local>
2019-05-08 17:59:56 +0200

    UI prototype

--- find_movie/ViewController.swift
+++ find_movie/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBarView: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let moviesList = ["Pierwszy", "Drugi", "Trzeci"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
